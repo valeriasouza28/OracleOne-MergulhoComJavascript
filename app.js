977624f3a69ac15d971a8da57578872b151326f8
valeriasouza28 <valleriasouza28@gmail.com>
2024-01-20 20:35:12 +0000

    add aula05

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ alert("Boasvindas ao jogo do número secreto");
 
 let chute;
 let tentativas = 1; 
-let numeroSecreto = parseInt(Math.random() *  100 + 1);
+let numeroMaximo = 100
+let numeroSecreto = parseInt(Math.random() *  numeroMaximo + 1);
 console.log(numeroSecreto);
 
 while(chute != numeroSecreto){
-    chute = prompt(`Escolha um número entre 1 e 100: `);
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}: `);
 // ns = 9 chute = 2 
     if (chute == numeroSecreto){
         break;
@@ -22,7 +23,7 @@ while(chute != numeroSecreto){
 }
 
 let palavraTentativa = tentativas > 1 ? "tentativas": "tentativa"
-alert(`Isso aí! Você descobriu o Nùmero Secreto ${numeroSecreto} 
-com ${tentativas} ${palavraTentativa}`)
+alert(`Isso aí! Você descobriu o Nùmero Secreto ${numeroSecreto}. 
+Com ${tentativas} ${palavraTentativa}`)
 
 
